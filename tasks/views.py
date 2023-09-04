a7950f438ff56c8629cd49d40cad6e6a73832056
jaggerv <javiervleon99@gmail.com>
2023-09-04 19:28:04 -0300

    Primeras funcionalidades

--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
 
 # Create your views here.
 def list_tasks(request):
-    return render(request, 'list_tasks.html')
+    tasks = Task.objects.all()
+    print(tasks)
+    return render(request, 'list_tasks.html', {"tasks": tasks})
+
+def create_task(request):
+    task = Task(title=request.POST['title'], description=request.POST['description'])
+    task.save()
+    return redirect('/tasks/')
+
+def delete_task(request, task_id):
+    print(task_id)
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('/tasks/')
